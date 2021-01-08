474d3ef680ef251012a5a302d1a5e3377f109453
MaxTGV <maxtugovlife@gmail.com>
2021-01-08 11:26:25 +0600

    fix bag commit - update login and registration page

--- src/shared/component/BaseButton.js
+++ src/shared/component/BaseButton.js
@@ -1,6 +1,5 @@
 import React, { forwardRef } from "react";
 import { Button } from "@material-ui/core";
-import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
